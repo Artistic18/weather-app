01654748e8f8d5c7fcfdff7707def3536263783d
Hrishit <hrishitbiswas20@gmail.com>
2024-11-20 03:58:40 +0530

    added tls alse

---
+++ server/weather-cache-service/src/main/java/com/sapient/weather_cache_service/config/RedisConfig.java
@@ -0,0 +1,10 @@
+package com.sapient.weather_cache_service.config;
+
+import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@EnableCaching
+public class RedisConfig {
+    
+}
